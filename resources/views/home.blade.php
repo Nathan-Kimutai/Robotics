e5bb5dea334ca8a0e29082b8da38f9b8d7cf24ec
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-04-29 00:48:32 +0300

    created a beautiful ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,22 +4,39 @@
 
 @section('content')
 <section id="banner">
-<div class="container">
-    <div class="row">
-        <div class="col-md-6">
-            <h2>Why robotics</h2>
-            <p>Robotics is the skill for the future, don't miss it out learn robotics</p>
-            <a class="btn intro" href="/register">Get Started</a>
+    <div class="overlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Why robotics</h2>
+                    <p>Robotics is the skill for the future, don't miss it out learn robotics</p>
+                    <a class="btn intro" href="/register">Get Started</a>
+            </div>
+            <div class="col-md-4"></div>
         </div>
-        <div class="col-md-4"></div>
     </div>
-</div>
 </section>
 <section id="showcase">
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-                <h3></h3>
+                <h3>Hands on experience</h3>
+                <p>In our robotics classes you will get hands on 
+                    experience by actually making and breaking stuff.
+                    You will get to work on so amazing projects that will blow your mind
+                </p>
+            </div>
+            <div class="col-md-4">
+                <h3>Careers guidance and growth</h3>
+                <p>During your course duration you will get career guidance and growth from our professional mentors 
+                    some of them who have worked in great comapnies like tesla and citadel
+                </p>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <h3>Speed and Teamwork</h3>
+                <p>This is the most amazing part where you will be able to compete for prizes. You will get to be in a group of three people where
+                    you will be working on solving a problem and the first team to solve the challenge gets the prize
+                </p>
             </div>
         </div>
     </div>
@@ -27,15 +44,6 @@
 <section id="main">
 
 </section>
-
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-            </div>
-        </div>
-    </div>
-</footer>
 @endsection
 
 
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,6 +11,40 @@
 	@include('shared.navbar')
 
 	@yield('content')
+
+	<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+				<h4>Robotics</h4>
+			</div>
+			<div class="col-md-11"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<ul>
+					<li><a href="/#aboutus">About us</a></li>
+					<li><a href="/#careers">Careers</a></li>
+					<li><a href="/#learning">Learning</a></li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<ul>
+					<li><a href="/#aboutus">About us</a></li>
+					<li><a href="/#careers">Careers</a></li>
+					<li><a href="/#learning">Learning</a></li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+			<ul>
+					<li><a href="/#aboutus">About us</a></li>
+					<li><a href="/#careers">Careers</a></li>
+					<li><a href="/#learning">Learning</a></li>
+				</ul>
+			</div>
+		</div>
+    </div>
+	</footer>
 	
 	<script type="text/javascript" src="{!! asset('js/jquery.js') !!}"></script>
 	<script type="text/javascript" src="{!! asset('js/bootstrap.js') !!}"></script>
