74184c02398e5fadecb3baa7d13b18216415580f
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-04-28 23:24:10 +0300

    added the image to the banner id

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,39 @@
 @section('title','Home')
 
 @section('content')
-	<div class="container">
+<section id="banner">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Why robotics</h2>
+            <p>Robotics is the skill for the future, don't miss it out learn robotics</p>
+            <a class="btn intro" href="/register">Get Started</a>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
+</section>
+<section id="showcase">
+    <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <h1 class="text-center">
-                    Leaning Laravel
-                </h1>
+            <div class="col-md-4">
+                <h3></h3>
             </div>
         </div>
-	</div>
+    </div>
+</section>
+<section id="main">
+
+</section>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+            </div>
+        </div>
+    </div>
+</footer>
 @endsection
 
 
--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-inverse">
-      <div class="container-fluid">
+<nav id="navbar" class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
             <div class="navbar-header">
                   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="navBar">
                         <span class="sr-only">Toggle navigation</span>
@@ -7,13 +7,13 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                   </button>
-                  <a class="navbar-brand" href="/">Learning Laravel</a>
+                  <a class="navbar-brand" href="/">Robotics</a>
             </div>
 
             <!-- Navbar right -->
             <div class="collapse navbar-collapse" id="navBar">
                   <ul class="nav navbar-nav navbar-right">
-                        <li class="active">
+                        <li>
                               <a href="/">Home</a>
                         </li>
                         <li>
@@ -23,8 +23,8 @@
                               <a href="/contact">Contact</a>
                         </li>
                         <!-- Dropdown -->
-                        <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        <!-- <li class="dropdown"> -->
+                              <!-- <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     Member
                                     <span class="caret"></span>
                               </a>
@@ -36,7 +36,7 @@
                                           <a href="/users/login">Login</a>
                                     </li>
                               </ul>
-                        </li>
+                        </li> -->
                   </ul>
             </div>
       </div>            
